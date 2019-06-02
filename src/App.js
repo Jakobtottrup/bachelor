41eea1ab62207972aec2d0d7a1566ed844f8553b
jakobtottrup <jakobtottrup@gmail.com>
2019-06-02 19:44:59 +0200

    push to git

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import 'chartjs-plugin-streaming';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import GraphRealtime from "./components/graph-realtime.component";
+import GraphCreate from "./components/graph-create.component";
+import GraphEdit from "./components/graph-edit.component";
+import GraphList from "./components/graph-list.component";
+import GraphProfile from "./components/graph-profile";
+
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <Link to="/" className="navbar-brand">Home</Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+                                aria-expanded="false" aria-label="Toggle navigation"><span
+                            className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                            <ul className="navbar-nav mr-auto">{/*
+                                <li className="nav-item">
+                                    <Link to="/graph" className="nav-link">Realtime Data</Link>
+                                </li>*/}
+                                <li className="nav-item">
+                                    <Link to="/create" className="nav-link">Add Graph Connection</Link>
+                                </li>{/*
+                                <li className="nav-item">
+                                    <Link to="/edit/:id" className="nav-link">Edit Wemos Connection</Link>
+                                </li>*/}
+                            </ul>
+                        </div>
+
+                    </nav>
+
+                    <Route path="/" exact component={GraphList}/>
+                    <Route path="/edit/:id" component={GraphEdit}/>
+                    <Route path="/create" component={GraphCreate}/>
+                    <Route path="/graph/:id" component={GraphRealtime}/>
+                    <Route path="/display_profile/:id" component={GraphProfile}></Route>
+
+
+                </div>
+
+
+            </Router>
+        );
+    }
 }
 
 export default App;
